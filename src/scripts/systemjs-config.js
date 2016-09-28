a8e5b92fc4d175024183aa2df1527627ad42e29b
sguzunov <sguzunov@gmail.com>
2016-09-28 12:31:59 +0300

    Configured Systemjs to load all dependencies.

--- src/scripts/systemjs-config.js
+++ src/scripts/systemjs-config.js
@@ -3,9 +3,10 @@ SystemJS.config({
     transpiler: 'plugin-babel',
     // tell SystemJS where to look for the dependencies
     map: {
-        'plugin-babel': '../../node_modules/systemjs-plugin-babel/plugin-babel.js',
-        'systemjs-babel-build': '../../node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
-        // app start script
-        'main': './main.js'
+        'plugin-babel': '../node_modules/systemjs-plugin-babel/plugin-babel.js',
+        'systemjs-babel-build': '../node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
+        'jquery': '../bower-components/jquery/dist/jquery.js',
+        'handlebars': '../bower-components/handlebars/handlebars.js',
+        'main': './scripts/main.js'
     }
 });
