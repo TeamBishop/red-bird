50917b527c5b7b77d4c566416ec48f9699946403
pavelhristov <pavel.hristov_@abv.bg>
2016-09-29 15:08:32 +0300

    some routing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,15 @@ import { hasLoggedInUser } from 'userService';
 
 let app = new Sammy('#container', function() {
     this.get('#/', function() {
-        if (!hasLoggedInUser()) {
-
+        if (hasLoggedInUser()) {
+            this.redirect('#/home');
         } else {
-
+            this.redirect('#/authorise');
         }
     });
 
     this.get('#/home', function() {
-        this.redirect('/#/');
+        // home templete load
     });
 });
 
